2d9c6c98259577f5b4984f76e565dfb479e5d2c4
sanskriti1701 <sans.sg1701@gmail.com>
2023-11-09 22:30:44 +0530

    sign up

---
+++ src - Copy/pages/FoundaPorter.tsx
@@ -0,0 +1,125 @@
+import React from 'react'
+
+function FoundaPorter() {
+  return (
+    <div style={{'backgroundColor':'RGB(244, 63, 94)'}}  className='h-screen w-full flex flex-col justify-center items-center'>
+      
+<div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+<h1 className="font-semibold text-4xl text-center text-white mb-9">Found Customers</h1>
+
+    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" className="px-6 py-3">
+                Customer Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Photo
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Pick Up
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Drop
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Fair
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Sundar Lal
+                </th>
+                <td className="px-6 py-4">
+                <img className="w-10 h-10 rounded-full" src="https://thumbs.dreamstime.com/z/jongensavatar-ronde-die-webknoop-op-wit-wordt-ge%C3%AFsoleerd-105543227.jpg?w=768" alt="Rounded avatar"/>
+                </td>
+                <td className="px-6 py-4">
+                    RKMP, Platform 1
+                </td>
+                <td className="px-6 py-4">
+                RKMP, Platform 4
+                </td>
+                <td className="px-6 py-4">
+                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Rs 200</a>
+                </td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Sundar Lal
+                </th>
+                <td className="px-6 py-4">
+                <img className="w-10 h-10 rounded-full" src="https://thumbs.dreamstime.com/z/jongensavatar-ronde-die-webknoop-op-wit-wordt-ge%C3%AFsoleerd-105543227.jpg?w=768" alt="Rounded avatar"/>
+                </td>
+                <td className="px-6 py-4">
+                    RKMP, Platform 1
+                </td>
+                <td className="px-6 py-4">
+                RKMP, Platform 4
+                </td>
+                <td className="px-6 py-4">
+                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Rs 200</a>
+                </td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Sundar Lal
+                </th>
+                <td className="px-6 py-4">
+                <img className="w-10 h-10 rounded-full" src="https://thumbs.dreamstime.com/z/jongensavatar-ronde-die-webknoop-op-wit-wordt-ge%C3%AFsoleerd-105543227.jpg?w=768" alt="Rounded avatar"/>
+                </td>
+                <td className="px-6 py-4">
+                    RKMP, Platform 1
+                </td>
+                <td className="px-6 py-4">
+                RKMP, Platform 4
+                </td>
+                <td className="px-6 py-4">
+                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Rs 200</a>
+                </td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Sundar Lal
+                </th>
+                <td className="px-6 py-4">
+                <img className="w-10 h-10 rounded-full" src="https://thumbs.dreamstime.com/z/jongensavatar-ronde-die-webknoop-op-wit-wordt-ge%C3%AFsoleerd-105543227.jpg?w=768" alt="Rounded avatar"/>
+                </td>
+                <td className="px-6 py-4">
+                    RKMP, Platform 1
+                </td>
+                <td className="px-6 py-4">
+                RKMP, Platform 4
+                </td>
+                <td className="px-6 py-4">
+                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Rs 200</a>
+                </td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Sundar Lal
+                </th>
+                <td className="px-6 py-4">
+                <img className="w-10 h-10 rounded-full" src="https://thumbs.dreamstime.com/z/jongensavatar-ronde-die-webknoop-op-wit-wordt-ge%C3%AFsoleerd-105543227.jpg?w=768" alt="Rounded avatar"/>
+                </td>
+                <td className="px-6 py-4">
+                    RKMP, Platform 1
+                </td>
+                <td className="px-6 py-4">
+                RKMP, Platform 4
+                </td>
+                <td className="px-6 py-4">
+                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Rs 200</a>
+                </td>
+            </tr>
+            
+        </tbody>
+    </table>
+</div>
+
+    </div>
+  )
+}
+
+export default FoundaPorter
---
+++ src - Copy/pages/index.tsx
@@ -0,0 +1,24 @@
+import Contact from "../components/Contact";
+import { Hero } from "../components/Hero";
+import Layout from "../components/Layout";
+import Pricing from "../components/Pricing";
+import { Features } from "../components/Features";
+import { Services } from "../components/Services";
+import Faq from "../components/Faq";
+import Customers from "../components/Customers";
+import Link from "next/link";
+import SignUp from "@/pages/SignUp";
+
+export default function Home() {
+  return (
+    <Layout>
+      <Hero />
+      <Services />
+      <Features />
+      <Pricing />
+      <Faq />
+      <Contact />
+      <Customers />
+    </Layout>
+  );
+}
